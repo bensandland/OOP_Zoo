8c0d703636033f4123df72dc2faaf655426becfb
kristian <kristian.lind@live.dk>
2017-11-22 22:54:31 +0100

    Added some Fields & Properties

--- Zoo/Zoo/Animal.cs
+++ Zoo/Zoo/Animal.cs
@@ -7,5 +7,24 @@ namespace Zoo
 {
     public class Animal
     {
+        /*
+         * Internal values.
+         */
+
+        protected string _name;
+
+        /*
+         * Get-setters of variables. (remove either get or set in order to disable getting or setting.) if you want both, change to private.
+         */
+
+        public string name { get { return _name; } set { _name = value; } }
+
+        /*
+         * Constructor
+         */
+        public Animal()
+        {
+
+        }
     }
 }
--- Zoo/Zoo/Zoo.cs
+++ Zoo/Zoo/Zoo.cs
@@ -7,5 +7,16 @@ namespace Zoo
 {
     public class Zoo
     {
+        //List all private variables
+        private List<Animal> _animals;
+
+        //List all get-setters.
+        public List<Animal> animals { get { return _animals; } set { _animals = value; } }
+
+        public Zoo()
+        {
+            animals.Add(new Tiger()); // Yay my science worked. The cosine, of your route, divided by... #Heimerdonger #
+            animals.FirstOrDefault(animal.name == "John");
+        }
     }
 }
