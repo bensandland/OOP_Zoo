65747733fb6110ca84064baf4898b52a53882872
kristian <kristian.lind@live.dk>
2017-11-22 21:14:49 +0100

    Class Diagram WIP

---
+++ Zoo/Zoo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zoo zoo = new Zoo();
+            /* Hold alt (console) menu relateret arbejde inde 
+             * i denne class eller i en menu class.
+             * På den måde kan vi let porte det over til et
+             * hvilket som helst framework såsom:
+             * Forms, WPF, ASP.net etc.
+             */
+            Console.WriteLine(zoo.ToString());
+            Console.ReadKey();
+        }
+    }
+}
