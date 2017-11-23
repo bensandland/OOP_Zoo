40320d2ba75e86705c2e11589d9906c1d93ad55f
Fumlefinger <jann2273@gmail.com>
2017-11-23 13:30:50 +0100

    sad

--- Zoo/Zoo/Animals/Amphibian/Salamander.cs
+++ Zoo/Zoo/Animals/Amphibian/Salamander.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Zoo.Animals.Amphibian
+namespace Zoo
 {
-    class Salamander
+    public class Salamander : Amphibian
     {
+
+
+
     }
 }
