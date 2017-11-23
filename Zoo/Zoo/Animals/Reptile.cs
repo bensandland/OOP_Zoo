d0d83f8f00315b80c10daaa441eade4d2272da97
Fumlefinger <jann2273@gmail.com>
2017-11-23 09:47:51 +0100

    reptiles variabler sat op plus names.txt

--- Zoo/Zoo/Animals/Reptile.cs
+++ Zoo/Zoo/Animals/Reptile.cs
@@ -8,6 +8,14 @@ namespace Zoo
     public class Reptile : Animal
     {
 
+        private bool _canSwim;
+        private bool _isDeadly;
+        private bool _isColdBlooded;
+
+        public bool canSwim { get { return _canSwim; } set { _canSwim = value; } }
+        public bool isDeadly { get { return _isDeadly; } set { _isDeadly = value; } }
+        public bool isColdBlooded { get { return _isColdBlooded; } set { _isColdBlooded = value; } }
+
 
         public Reptile()
         {
