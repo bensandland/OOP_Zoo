36b5465d3b412811d79d91d3e5893989c42423d5
yoropiata <kristian.lind@live.dk>
2017-11-23 14:26:20 +0100

    Comments til rettelse af default value.

--- Zoo/Zoo/Animals/Bird/Penguin.cs
+++ Zoo/Zoo/Animals/Bird/Penguin.cs
@@ -9,8 +9,8 @@ namespace Zoo
     {
         public Penguin()
         {
-            name = "Tine";
-            species = this.GetType().Name;
+            name = "Tine"; // skal fjernes og sættes når man laver Penguin, så man ikke ender ud med 200 Pingviner som hedder Tine <3.
+            species = GetType().Name;
             canFly = true;
             GenerateWingspan(76.2, 81.4);
             GenerateWeight(12.1, 25.1);
