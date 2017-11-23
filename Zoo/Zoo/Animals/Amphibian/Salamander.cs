7229859eb2cc1e84c669ba13cb4e16642db8121e
Fumlefinger <jann2273@gmail.com>
2017-11-23 11:29:27 +0100

    forkost pause push

--- Zoo/Zoo/Animals/Amphibian/Salamander.cs
+++ Zoo/Zoo/Animals/Amphibian/Salamander.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace Zoo
+namespace Zoo.Animals.Amphibian
 {
-    public class Scorpion : Amphibian
+    class Salamander
     {
     }
 }
